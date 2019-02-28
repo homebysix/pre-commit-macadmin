1897e3f749e407a94b31501483cc545fe0a65dfb
Elliot Jordan <elliot@elliotjordan.com>
2019-02-27 23:17:57 -0800

    Parse in main() so we can capture return value

--- pre_commit_hooks/check_autopkg_recipe_list.py
+++ pre_commit_hooks/check_autopkg_recipe_list.py
@@ -24,49 +24,6 @@ def build_argument_parser():
     return parser
 
 
-def parse_text(filename):
-    """Parse recipe list in text format."""
-    with open(filename, "r") as openfile:
-        recipes = [
-            line
-            for line in openfile.read().splitlines()
-            if line and not line.startswith("#")
-        ]
-    return recipes
-
-
-def parse_plist(filename):
-    """Parse recipe list in plist format."""
-    try:
-        recipes = plistlib.readPlist(filename)
-    except (ExpatError, ValueError) as err:
-        print("{}: plist parsing error: {}".format(filename, err))
-        retval = 1
-    return []
-
-
-def parse_yml(filename):
-    """Parse recipe list in yml format."""
-    if YAML_INSTALLED:
-        with open(filename, "r") as openfile:
-            recipes = yaml.load(openfile)
-    else:
-        with open(filename, "r") as openfile:
-            recipes = [
-                line.lstrip("- ")
-                for line in openfile.read().splitlines()
-                if line and not line.startswith("#")
-            ]
-    return recipes
-
-
-def parse_json(filename):
-    """Parse recipe list in json format."""
-    with open(filename, "r") as openfile:
-        recipes = json.load(openfile)
-    return recipes
-
-
 def main(argv=None):
     """Main process."""
 
@@ -77,22 +34,49 @@ def main(argv=None):
     retval = 0
     for filename in args.filenames:
         if filename.endswith(".txt"):
-            recipe_list = parse_text(filename)
+            with open(filename, "r") as openfile:
+                recipe_list = [
+                    line
+                    for line in openfile.read().splitlines()
+                    if line and not line.startswith("#")
+                ]
         elif filename.endswith(".plist"):
-            recipe_list = parse_plist(filename)
-        elif filename.endswith((".yaml", ".yml")):
-            recipe_list = parse_yml(filename)
-        elif filename.endswith(".json"):
-            recipe_list = parse_json(filename)
-
-    if not recipe_list or not isinstance(recipe_list, list):
-        print("{}: invalid recipe list")
-        retval = 1
-    else:
-        if any((".munki" in recipe for recipe in recipe_list)):
-            if "MakeCatalogs" not in recipe_list[-1]:
-                print("{}: MakeCatalogs should be the last item in the list")
+            try:
+                recipe_list = plistlib.readPlist(filename)
+            except (ExpatError, ValueError) as err:
+                print("{}: plist parsing error: {}".format(filename, err))
                 retval = 1
+        elif filename.endswith((".yaml", ".yml")):
+            try:
+                if YAML_INSTALLED:
+                    with open(filename, "r") as openfile:
+                        recipe_list = yaml.load(openfile)
+                else:
+                    with open(filename, "r") as openfile:
+                        recipe_list = [
+                            line.lstrip("- ")
+                            for line in openfile.read().splitlines()
+                            if line and not line.startswith("#")
+                        ]
+            except Exception as err:
+                print("{}: yaml parsing error: {}".format(filename, err))
+                retval = 1
+        elif filename.endswith(".json"):
+            try:
+                with open(filename, "r") as openfile:
+                    recipe_list = json.load(openfile)
+            except Exception as err:
+                print("{}: json parsing error: {}".format(filename, err))
+                retval = 1
+
+        if not recipe_list or not isinstance(recipe_list, list):
+            print("{}: invalid recipe list")
+            retval = 1
+        else:
+            if any((".munki" in recipe for recipe in recipe_list)):
+                if "MakeCatalogs" not in recipe_list[-1]:
+                    print("{}: MakeCatalogs should be the last item in the list")
+                    retval = 1
 
     return retval
 
