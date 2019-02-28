22adcce74291b0fb5de5a339568677d3f87f4f7d
Elliot Jordan <elliot@elliotjordan.com>
2019-02-27 18:34:21 -0800

    Built out check-autopkg-recipes hook

--- pre_commit_hooks/check_autopkg_recipes.py
+++ pre_commit_hooks/check_autopkg_recipes.py
@@ -1,9 +1,47 @@
 #!/usr/bin/python
+"""This hook checks AutoPkg recipes to ensure they contain required top-level keys.
+(https://github.com/autopkg/autopkg/wiki/Recipe-Format)."""
+
+import argparse
+import plistlib
+from xml.parsers.expat import ExpatError
+
+
+def build_argument_parser():
+    """Build and return the argument parser."""
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
+    return parser
 
 
 def main(argv=None):
     """Main process."""
-    print("Hello world!")
+
+    # Top level keys that all AutoPkg recipes should contain.
+    required_keys = ("Identifier", "Input")
+
+    # Parse command line arguments.
+    argparser = build_argument_parser()
+    args = argparser.parse_args(argv)
+
+    retval = 0
+    for filename in args.filenames:
+        try:
+            recipe = plistlib.readPlist(filename)
+            for req_key in required_keys:
+                if req_key not in recipe:
+                    print("{}: missing required key {}".format(filename, req_key))
+                    retval = 1
+                    break  # No need to continue checking this file
+
+        except ExpatError as err:
+            print(err)
+            retval = 1
+
+    return retval
 
 
 if __name__ == "__main__":
