f4cc26edb8f6871fb922f956a6e9fb67abe3eae7
Elliot Jordan <elliot@elliotjordan.com>
2019-02-27 21:33:24 -0800

    Built out check-munkiadmin-scripts hook

--- pre_commit_hooks/check_munkiadmin_scripts.py
+++ pre_commit_hooks/check_munkiadmin_scripts.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python
+"""This hook ensures MunkiAdmin scripts are executable."""
+
+import argparse
+import os
+
+
+def build_argument_parser():
+    """Build and return the argument parser."""
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
+    return parser
 
 
 def main(argv=None):
     """Main process."""
-    print("Hello world!")
+
+    # Parse command line arguments.
+    argparser = build_argument_parser()
+    args = argparser.parse_args(argv)
+
+    retval = 0
+    for filename in args.filenames:
+        if not os.access(filename, os.X_OK):
+            print("{}: not executable".format(filename))
+            retval = 1
+
+    return retval
 
 
 if __name__ == "__main__":
