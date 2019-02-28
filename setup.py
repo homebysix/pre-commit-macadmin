90c346539b10641ea715602583079614f19ca8c9
Elliot Jordan <elliot@elliotjordan.com>
2019-02-27 21:36:50 -0800

    Removed check-munki-manifests hook

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
         "console_scripts": [
             "check-autopkg-recipe-list = pre_commit_hooks.check_autopkg_recipe_list:main",
             "check-autopkg-recipes = pre_commit_hooks.check_autopkg_recipes:main",
-            "check-munki-manifests = pre_commit_hooks.check_munki_manifests:main",
             "check-munki-pkgsinfo = pre_commit_hooks.check_munki_pkgsinfo:main",
             "check-munkiadmin-scripts = pre_commit_hooks.check_munkiadmin_scripts:main",
             "check-munkipkg-buildinfo = pre_commit_hooks.check_munkipkg_buildinfo:main",
